089de205effee01be3fe97e201f3faa98346538a
Jordan <bettercalljo@gmail.com>
2017-11-24 08:46:09 +0100

    Add files via upload

---
+++ commentaires.php
@@ -0,0 +1,39 @@
+<?php
+
+try
+{
+    $bddblog = new PDO('mysql:host=localhost;dbname=bddblog;charset=utf8', 'root','');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>COMMENTAIRES</title>
+        <link rel="stylesheet" href="commentaires.css">
+    </head>
+
+<body>
+
+    <h1>SECTION COMMENTAIRES</h1>
+
+        <form action="commentaires_post.php" method="post">
+
+    Veuillez saisir votre pseudo : <input type ="text" name="auteur"><br><br>
+    Veuillez saisir votre commentaire : <input type ="text" name="commentaire"><br><br><br>
+
+            <input type="hidden" name="id_billet" value="<?php echo $_GET['id']?>">
+
+    <INPUT TYPE="submit" NAME="validation" VALUE="VALIDER LE COMMENTAIRE">
+
+        </form>
+
+
+</body>
+
+</html>
---
+++ index_post.php
@@ -0,0 +1,27 @@
+<?php
+
+$titre = $_POST['titre_billet'];
+$contenu =$_POST['texte_billet'];
+
+try
+{
+    $bddblog = new PDO('mysql:host=localhost;dbname=bddblog;charset=utf8', 'root','');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+
+$stmt = $bddblog->prepare('INSERT INTO billets(titre, contenu) VALUES (:titre, :contenu)'); //preparation de la requete
+
+$stmt ->bindParam('titre', $titre, PDO::PARAM_STR );                                // lie les variable avec item bdd
+$stmt ->bindParam('contenu', $contenu, PDO::PARAM_STR );
+
+try {
+    $stmt->execute();
+} catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+header('Location: index.php');
+?>
